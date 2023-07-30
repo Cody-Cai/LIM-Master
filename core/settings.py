92face5e15b72a88124193b8a6eb1c42b8b7c126
Cody Cai <cai0754@163.com>
2023-07-31 02:19:48 +0800

    launch

--- core/settings.py
+++ core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'lim_master.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lim_master.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
